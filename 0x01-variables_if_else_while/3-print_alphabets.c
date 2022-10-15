b6471de3740c538f783e6707542cbcb5427b7e3a
petersgabriel <Petersgabriel0447@gmail.com>
2022-10-14 19:00:55 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,6 +4,7 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	char c;
@@ -18,7 +19,7 @@ int main(void)
 			c++;
 		}
 	while
-		(d <= 'z') {
+		(d <= 'Z') {
 			putchar(d);
 			d++;
 		}
