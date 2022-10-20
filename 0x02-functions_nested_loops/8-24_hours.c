ea72302e976748064c107717a8edfd9e7f8cab5f
petersgabriel <Petersgabriel0447@gmail.com>
2022-10-19 18:44:48 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,13 +12,12 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for (hour = 0; hour <= 23; hour++)
+		for (minute = 0; minute <= 23; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
 			_putchar((minute % 10) + '0');
 		}
 	}
